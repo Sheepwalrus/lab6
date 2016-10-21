d34005883b5ca41759e4eec9c146e6da6a6aeff8
Josh <jdphd7@gmail.com>
2016-10-21 20:42:05 +0000

    Pushed merged copy onto master

--- lab6.cpp
+++ lab6.cpp
@@ -2,6 +2,78 @@
 
 using namespace std;
 
+<<<<<<< lab6.cpp
+
+  enum class bookStatus{open,closed};
+  enum class lampStatus{on,off};
+
+
+
+    class book
+
+    {
+
+
+     int numofPages;
+     double width,length;
+     string title;
+     double area;
+     bookStatus status;
+     void calcArea()
+     {
+        area = length * width;
+     }
+
+
+
+        book (bookStatus tempstatus, double tempnumofPages, double tempwidth, double templength, string temptitle)
+        {
+
+           numofPages = tempnumofPages;
+           width = tempwidth;
+           status = tempstatus;
+           length = templength;
+           title = temptitle;
+
+           if (status == bookStatus::open)
+           {
+               length = length * 2;
+           }
+
+
+        }
+
+        ~book(){};
+
+    };
+
+
+    class lamp
+    {
+        string color;
+        double width, length, area;
+        lampStatus status;
+        void calcArea()
+        {
+
+            area = width * length;
+        };
+
+            lamp (string tempcolor, double tempwidth, double templength, lampStatus tempstatus)
+                {
+
+                    color = tempcolor;
+                    width = tempwidth;
+                    length = templength;
+                    status = tempstatus;
+                    calcArea();
+
+                }
+
+                ~lamp(){};
+
+    };
+=======
 enum class material{wood,steel,plastic};
 
 class table
@@ -67,5 +139,6 @@ int main()
 	//} 
 	return 0;
 }
+>>>>>>> lab6eric.cpp
 
 
